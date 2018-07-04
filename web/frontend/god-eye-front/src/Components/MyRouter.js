0733a7d6d92ee3813415c9d7e1d6629064721a4b
zhaoying98sjtu <zhaoying98@sjtu.edu.cn>
2018-07-04 08:51:05 +0800

    router

--- web/frontend/god-eye-front/src/Components/MyRouter.js
+++ web/frontend/god-eye-front/src/Components/MyRouter.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Home from './Home.js';
 import { Switch } from 'react-router';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import traceTarget from "./TraceTarget";
+import HistoryVideo from "./HistoryVideo";
+import Settings from "./Settings";
 
 class MyRouter extends React.Component {
     render(){
@@ -10,8 +13,8 @@ class MyRouter extends React.Component {
                 <div>
                     <Switch>
                         <Route exact path="/home" component={Home} />
-                        <Route exact path="/current-video" component={currentVideo} />
-                        <Route exact path="/history-video" component={historyVideo} />
+                        <Route exact path="/current-video" component={CurrentVideo} />
+                        <Route exact path="/history-video" component={HistoryVideo} />
                         <Route exact path="/trace-target"  component={traceTarget} />
                         <Route exact path="/settings"  component={Settings} />
                     </Switch>
---
+++ web/frontend/god-eye-front/src/Components/Settings.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Button } from 'antd';
+import { Layout,Menu,Icon,Input } from 'antd';
+import { Link } from "react-router-dom";
+
+
+class Settings extends React.Component {
+    render(){
+        return(
+          <div></div>
+        );
+    }
+}
+
+export default Settings;
