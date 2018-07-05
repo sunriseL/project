7aa3af4bda90f86e5cc578e891bc92f2499a4d4e
Kurotsuba <kanneco98@gmail.com>
2018-07-05 14:50:51 +0800

    change layout

--- web/frontend/god-eye-front/src/Components/Settings.js
+++ web/frontend/god-eye-front/src/Components/Settings.js
@@ -1,26 +1,8 @@
 import React from 'react';
-import {  Modal,Layout, Upload, Icon, message, Breadcrumb } from 'antd';
+import {  Modal,Layout, Upload, Icon, Breadcrumb, Row, Col } from 'antd';
 import UserMap from './UserMap';
 
-const uploadProps = {
-    name: 'file',
-    multiple: false,
-    action: 'localhost:8090',
-    onChange(info) {
-        const status = info.file.status;
-        if (status !== 'uploading') {
-            console.log(info.file, info.fileList);
-        }       
-        if (status === 'done') {
-            message.success(`${info.file.name} file uploaded successfully.`);
-        } else if (status === 'error') {
-            message.error(`${info.file.name} file upload failed.`);
-        }
-    },
-}
-
 const {  Content } = Layout;
-const Dragger = Upload.Dragger;
 
 class Settings extends React.Component {
     constructor() {
@@ -64,29 +46,24 @@ class Settings extends React.Component {
                     </Breadcrumb>
                 </Layout>
                 <Content>
-                    <Dragger {...uploadProps}>
-                        <p className="ant-upload-drag-icon">
-                            <Icon type="inbox" />
-                        </p>
-                        <p className="ant-upload-drag-text">上传地图</p>
-                        <p className="ant-upload-drag-hint">点击或拖拽上传地图图片</p>
-                    </Dragger>
+                    <Row><Col offset={8}>
+                        <div className="div1">
+                            <Upload
+                                style={{maxWidth:2160, width: '90%', margin: '5%',height: '200%'}}
+                                action="//jsonplaceholder.typicode.com/posts/"
+                                listType="picture"
+                                fileList={this.state.fileList}
+                                onPreview={this.handlePreview}
+                                onChange={this.handleChange}
+                            >
+                                {this.state.fileList.length >= 1 ? null : uploadButton}
+                            </Upload>
+                            <Modal visible={this.state.previewVisible} footer={null} onCancel={this.handleCancel}>
+                                <img alt="example" style={{ width: '500%' ,height: '500%'}} src={this.state.previewImage} />
+                            </Modal>
+                        </div>
+                    </Col></Row>
                     <UserMap />
-                    <div className="div1">
-                        <Upload
-                            style={{maxWidth:2160, width: '90%', margin: '5%',height: '200%'}}
-                            action="//jsonplaceholder.typicode.com/posts/"
-                            listType="picture"
-                            fileList={this.state.fileList}
-                            onPreview={this.handlePreview}
-                            onChange={this.handleChange}
-                        >
-                            {this.state.fileList.length >= 1 ? null : uploadButton}
-                        </Upload>
-                        <Modal visible={this.state.previewVisible} footer={null} onCancel={this.handleCancel}>
-                            <img alt="example" style={{ width: '500%' ,height: '500%'}} src={this.state.previewImage} />
-                        </Modal>
-                    </div>
                 </Content>
             </Layout>
         );
