15be0f5fd5ba61e46787138657dafabf4cacb3fa
zhaoying98sjtu <zhaoying98@sjtu.edu.cn>
2018-07-05 11:23:45 +0800

    zy frontend modified

--- web/frontend/god-eye-front/src/Components/CurrentVideo.js
+++ web/frontend/god-eye-front/src/Components/CurrentVideo.js
@@ -7,7 +7,7 @@ class CurrentVideo extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            videoLink: "https://mvpc.eastday.com/vgaoxiao/20180703/20180703175205896698808_1_06400360.mp4"
+            videoLink: "http://mvpc.eastday.com/vtiyu/20170907/20170907095753399577898_1_06400360.mp4"
         };
 
     }
@@ -21,8 +21,8 @@ class CurrentVideo extends React.Component {
                     </Breadcrumb>
                 </Layout>
                 <Content>
-                    <video height="600" width="800" controls="controls" preload="true">
-                        <source src= { this.state['videoLink'] } type="video/mp4" /> 
+                    <video height="600" width="800" controls="controls" preload={true}>
+                        <source src= { this.state['videoLink'] } type="video/mp4" />
                         <source src= { this.state['videoLink'] } type="video/ogg" /> 
                         <source src= { this.state['videoLink'] } type="video/webm" />
                         <object data = { this.state['videoLink'] } >
--- web/frontend/god-eye-front/src/Components/UserMap.js
+++ web/frontend/god-eye-front/src/Components/UserMap.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import {Card } from 'antd';
 import { withStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
+import pic from '../image/usermap.jpg'
+//import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
@@ -16,12 +18,14 @@ const styles = {
     },
 };
 
+let url ='../image/usermap.jpg';
+
 class UserMap extends React.Component{
     constructor(props){
         super(props);
 
         this.state = {
-            map_url:"../../usermap.jpg",
+            map_url:'../image/usermap.jpg',
             // Put UserMap Here
             map_name: "用户地图"
         }
@@ -31,15 +35,15 @@ class UserMap extends React.Component{
             <div>
                 <Card style={{maxWidth:2160, width: '60%'}}>
                     <CardMedia
-                    style={{height: '60%', paddingTop: '56.25%'}}
-                    image={ this.state['map_url'] }
-                    title={ this.state['map_name'] }
+                        style={{height: '60%', paddingTop: '56.25%'}}
+                        image={ this.state['map_url'] }
+                        title={ this.state['map_name'] }
                     />
-                        <CardContent>
-                            <Typography gutterBottom variant="headline" component="h2">
-                                { this.state['map_name'] }
-                            </Typography>
-                        </CardContent>
+                    <CardContent>
+                        <Typography gutterBottom variant="headline" component="h2">
+                            { this.state['map_name'] }
+                        </Typography>
+                    </CardContent>
                 </Card>
             </div>
         );
