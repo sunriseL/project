69e871345ee71524191c47b632aca0b8dfd17aa6
zhaoying98sjtu <zhaoying98@sjtu.edu.cn>
2018-07-10 15:51:19 +0800

    camera

--- web/frontend/god-eye-front/src/Components/CurrentVideo.js
+++ web/frontend/god-eye-front/src/Components/CurrentVideo.js
@@ -5,8 +5,48 @@ import VideoPlayer from './VideoPlayer.js';
 import Grid from '@material-ui/core/Grid';
 
 const { Footer } = Layout;
+let video,canvas,capture,context;
+function getUserMediaToPhoto(constraints,success,error) {
+    if(navigator.mediaDevices.getUserMedia){
+        //最新标准API
+        navigator.mediaDevices.getUserMedia(constraints).then(success).catch(error);
+    }else if (navigator.webkitGetUserMedia) {
+        //webkit核心浏览器
+        navigator.webkitGetUserMedia(constraints,success,error);
+    }else if(navigator.mozGetUserMedia){
+        //firefox浏览器
+        navigator.mozGetUserMedia(constraints,success,error);
+    }else if(navigator.getUserMedia){
+        //旧版API
+        navigator.getUserMedia(constraints,success,error);
+    }
+}
+//成功回调函数
+function success(stream){
+    //兼容webkit核心浏览器
+    var CompatibleURL = window.URL || window.webkitURL;
+    //将视频流转化为video的源
+    video.src = CompatibleURL.createObjectURL(stream);
+    video.play();//播放视频
+}
+function error(error) {
+    console.log('访问用户媒体失败：',error.name,error.message);
+}
+
 
 class CurrentVideo extends React.Component {
+    componentDidMount(){
+        video = document.getElementById('video');
+        canvas = document.getElementById('canvas');
+        capture = document.getElementById('capture');
+        context = canvas.getContext('2d');
+        if (navigator.mediaDevices.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.getUserMedia) {
+            getUserMediaToPhoto({video: {width: 480, height: 320}}, success, error);
+        } else {
+            alert('你的浏览器不支持访问用户媒体设备');
+        }
+        context.drawImage(video, 0, 0, 480, 320);
+    }
     render(){
         return(
             <Layout>
@@ -23,10 +63,11 @@ class CurrentVideo extends React.Component {
                             <UserMap /> 
                     </Grid>
                     <Grid item xs>
-                            <VideoPlayer />
+                        <video id="video" width="640" height="480" autoplay></video>
+                        <button id="snap">拍照</button>
+                        <canvas id="canvas" width="640" height="480"></canvas>
                     </Grid>
                 </Grid>
-
                 <Footer style={{ textAlign: 'center' }}>
                     God Eye ©2018 Created by SunriseL Team
                 </Footer>
--- web/frontend/god-eye-front/src/Components/Test.js
+++ web/frontend/god-eye-front/src/Components/Test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Button } from 'antd';
 import $ from 'jquery';
 
+
 class Test extends React.Component {
     constructor() {
         super();
@@ -10,36 +11,16 @@ class Test extends React.Component {
         }
     }
 
-    test(){
-      /*  fetch('http://localhost:8081/test', {
-                method: 'POST',
-                mode: 'cors',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ name: 'abc' }),
-        }).then(response =>{
-            console.log("Request Successful", response);
-            return response.json()
-                .then(result =>{
-                    console.log(result[0]);
-                })
-            })*/
-            $.ajax({
-                type: "get",
-                url: "http://127.0.0.1:8081/get_new_map",
-                crossDomain: true,
-                success: function (data) {
-                console.log(data);
-            },
-                error : function() {}
-            })
-    }
-
     render(){
         return(
            <div>
                <Button onClick={() => this.test()}> 跨域测试</Button>
+               <video id="video" width="480" height="320" controls>
+               </video>
+               <div>
+                   <button id="capture">拍照</button>
+               </div>
+               <canvas id="canvas" width="480" height="320"></canvas>
            </div>
         );
     }
