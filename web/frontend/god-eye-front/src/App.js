07eb913a76bd330f5942f8a03777f058c7f9a0f7
Kurotsuba <kanneco98@gmail.com>
2018-07-10 09:49:09 +0800

    optimize loading experience

--- web/frontend/god-eye-front/src/App.js
+++ web/frontend/god-eye-front/src/App.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
 import MainNav from './Components/MainNav';
+import $ from 'jquery';
 
 class App extends Component {
     ifDBEmpty(){
-        return 'false';
+        $.ajax({
+            type: "get",
+            url: "http://127.0.0.1:8081/has_map",
+            crossDomain: true,
+            success: function (data) {
+                if(data===0){
+                    return 'true';
+                }else{
+                    return 'false';
+                }
+            },
+            error : function() {
+                alert("something went wrong");
+            }
+        })
+
+        // return 'false';
     }
 
     componentDidMount(){
