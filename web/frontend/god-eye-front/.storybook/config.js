f603d886a173959b5a8caebb7fa6a8428403c429
Kurotsuba <kanneco98@gmail.com>
2018-07-25 10:04:25 +0800

    add storybook

--- web/frontend/god-eye-front/.storybook/config.js
+++ web/frontend/god-eye-front/.storybook/config.js
@@ -1,7 +1,9 @@
-import { configure } from '@storybook/react';
+import { configure, getStorybook } from '@storybook/react';
+
 
 function loadStories() {
   require('../src/stories');
 }
 
 configure(loadStories, module);
+
--- web/frontend/god-eye-front/src/stories/index.js
+++ web/frontend/god-eye-front/src/stories/index.js
@@ -1,24 +1,17 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
 
-import { Button, Welcome } from '@storybook/react/demo';
 
-import AddCamera from '../Components/AddCamera';
+import MainNav from '../Components/MainNav';
+import CurrentVideo from '../Components/CurrentVideo';
+import HistoryVideo from '../Components/HistoryVideo';
+import TraceTarget from '../Components/TraceTarget';
+import Settings from '../Components/Settings';
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
-
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ));
-
-  storiesOf('AddCamera', module)
-    .add('testTest', () => <AddCamera onClick={true} />);
+storiesOf('IT-snapshot-test', module)
+  .add('MainNav', ()=><MainNav />)
+  .add('CurrentVideo', ()=><CurrentVideo />)
+  .add('HistoryVideo', ()=><HistoryVideo />)
+  .add('TraceTarget', ()=><TraceTarget />)
+  .add('Settings', ()=><Settings />);
