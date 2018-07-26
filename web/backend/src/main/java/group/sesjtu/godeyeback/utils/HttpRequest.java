96f6c05dee31974818080fd4b944b46230fb79f4
Kurotsuba <kanneco98@gmail.com>
2018-07-26 15:49:13 +0800

    change timeout limit

--- web/backend/src/main/java/group/sesjtu/godeyeback/utils/HttpRequest.java
+++ web/backend/src/main/java/group/sesjtu/godeyeback/utils/HttpRequest.java
@@ -22,8 +22,7 @@ import java.util.concurrent.TimeUnit;
 public class HttpRequest {
     //OkHttpClient client = new OkHttpClient();
     OkHttpClient client = new OkHttpClient.Builder()
-                .connectTimeout(30, TimeUnit.SECONDS)
-                .readTimeout(30, TimeUnit.SECONDS)
+                .connectTimeout(300, TimeUnit.SECONDS)
                 .build();
     public static final MediaType JSON = MediaType.parse("application/x-www-form-urlencoded; charset=utf-8");
 
