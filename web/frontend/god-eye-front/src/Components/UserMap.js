2fc363244a065299a9bb632d68bc40ffe4f03159
Kurotsuba <kanneco98@gmail.com>
2018-07-26 10:48:59 +0800

    build

--- web/frontend/god-eye-front/src/Components/UserMap.js
+++ web/frontend/god-eye-front/src/Components/UserMap.js
@@ -3,7 +3,6 @@ import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
-import "./UserMap.css";
 import emitter from "../Utils/EventEmitter";
 import $ from 'jquery';
 
